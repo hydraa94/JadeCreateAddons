9f21801d091ca548e87c7748d57ff9fc593141ce
hydraa94 <nailuldev2948@gmail.com>
2026-06-15 19:49:49 +0700

    feat: add exact block targeting for contraptions

--- src/main/java/com/hydraa/jadecreateaddon/create/CreatePlugin.java
+++ src/main/java/com/hydraa/jadecreateaddon/create/CreatePlugin.java
@@ -1,26 +1,42 @@
 package com.hydraa.jadecreateaddon.create;
 
+import com.zurrtum.create.client.foundation.utility.RaycastHelper;
 import com.zurrtum.create.content.contraptions.AbstractContraptionEntity;
+import com.zurrtum.create.content.contraptions.Contraption;
+import com.zurrtum.create.content.decoration.placard.PlacardBlock;
+import com.zurrtum.create.content.equipment.armor.BacktankBlock;
+import com.zurrtum.create.content.equipment.armor.BacktankBlockEntity;
 import com.zurrtum.create.content.equipment.blueprint.BlueprintEntity;
 import com.zurrtum.create.content.fluids.tank.FluidTankBlockEntity;
 import com.zurrtum.create.content.kinetics.base.KineticBlockEntity;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.resources.Identifier;
-
-import net.minecraft.world.level.block.Block;
-
-import com.zurrtum.create.content.equipment.armor.BacktankBlockEntity;
+import com.zurrtum.create.content.processing.burner.BlazeBurnerBlock;
 import com.zurrtum.create.content.processing.burner.BlazeBurnerBlockEntity;
 
-import com.zurrtum.create.content.processing.burner.BlazeBurnerBlock;
-import com.zurrtum.create.content.decoration.placard.PlacardBlock;
-import com.zurrtum.create.content.equipment.armor.BacktankBlock;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.StructureBlockInfo;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult.Type;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import snownee.jade.api.Accessor;
+import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
+import snownee.jade.api.callback.JadeRayTraceCallback;
+import snownee.jade.api.config.IWailaConfig;
+import snownee.jade.impl.WailaClientRegistration;
+import snownee.jade.overlay.RayTracing;
 
 
 @WailaPlugin
@@ -37,6 +53,7 @@ public class CreatePlugin implements IWailaPlugin {
     public static final Identifier HIDE_BOILER_TANKS = Identifier.fromNamespaceAndPath(ID, "hide_boiler_tanks");
     public static final Identifier CRAFTING_BLUEPRINT = Identifier.fromNamespaceAndPath(ID, "crafting_blueprint");
     public static final Identifier CONTRAPTION_INVENTORY = Identifier.fromNamespaceAndPath(ID, "contraption_inv");
+    public static final Identifier CONTRAPTION_EXACT_BLOCK = Identifier.fromNamespaceAndPath(ID, "exact_block");
 
 
 
@@ -49,6 +66,7 @@ public class CreatePlugin implements IWailaPlugin {
         registration.registerBlockDataProvider(KineticDataProvider.INSTANCE, KineticBlockEntity.class);
         registration.registerItemStorage(ContraptionItemStorageProvider.INSTANCE, AbstractContraptionEntity.class);
         registration.registerFluidStorage(ContraptionFluidStorageProvider.INSTANCE, AbstractContraptionEntity.class);
+
     }
 
     @Override
@@ -56,6 +74,7 @@ public class CreatePlugin implements IWailaPlugin {
     public void registerClient(IWailaClientRegistration registration) {
         registration.addConfig(CreatePlugin.REQUIRES_GOGGLES, true);
         registration.addConfig(CreatePlugin.GOGGLES_DETAILED, false);
+        registration.addConfig(CreatePlugin.CONTRAPTION_EXACT_BLOCK, true);
         registration.registerBlockComponent(FilterProvider.INSTANCE, Block.class);
         registration.registerBlockComponent(BlazeBurnerProvider.INSTANCE, BlazeBurnerBlock.class);
         registration.registerBlockComponent(BacktankProvider.INSTANCE, BacktankBlock.class);
@@ -68,6 +87,47 @@ public class CreatePlugin implements IWailaPlugin {
         registration.registerItemStorageClient(CraftingBlueprintProvider.INSTANCE);
         registration.registerItemStorageClient(ContraptionItemStorageProvider.INSTANCE);
         registration.registerFluidStorageClient(ContraptionFluidStorageProvider.INSTANCE);
+        registration.registerEntityComponent(ContraptionExactBlockProvider.INSTANCE, AbstractContraptionEntity.class);
+        registration.registerEntityIcon(ContraptionExactBlockProvider.INSTANCE, AbstractContraptionEntity.class);
+
+//        EntityType.byString("create:super_glue").ifPresent(registration::hideTarget);
+
+
+
+        RayTracing.INSTANCE.entityFilter = RayTracing.INSTANCE.entityFilter.and(e -> {
+            if (!(e instanceof AbstractContraptionEntity contraptionEntity)) return true;
+            Minecraft mc = Minecraft.getInstance();
+            Entity camera = mc.getCameraEntity();
+            if (camera == null) return true;
+            float pt = mc.getDeltaTracker().getGameTimeDeltaPartialTick(true);
+            Vec3 origin = camera.getEyePosition(pt);
+            Vec3 lookVector = camera.getViewVector(pt);
+            float reach = (float)(mc.player.blockInteractionRange() + IWailaConfig.get().general().getExtendedReach());
+            Vec3 target = origin.add(lookVector.x * reach, lookVector.y * reach, lookVector.z * reach);
+            Vec3 localOrigin = contraptionEntity.toLocalVector(origin, 1);
+            Vec3 localTarget = contraptionEntity.toLocalVector(target, 1);
+            Contraption contraption = contraptionEntity.getContraption();
+            RaycastHelper.PredicateTraceResult predicateResult = RaycastHelper.rayTraceUntil(localOrigin, localTarget, p -> {
+                StructureBlockInfo blockInfo = contraption.getBlocks().get(p);
+                if (blockInfo == null) return false;
+                BlockState state = blockInfo.state();
+                VoxelShape shape = state.getShape(mc.level, BlockPos.ZERO);
+                if (shape.isEmpty()) return false;
+                BlockHitResult rayTrace = shape.clip(localOrigin, localTarget, p);
+                if (IWailaConfig.get().plugin().get(CreatePlugin.CONTRAPTION_EXACT_BLOCK) && rayTrace != null && rayTrace.getType() != Type.MISS) {
+                    BlockAccessor originalAccessor = registration.blockAccessor().blockState(state).hit(rayTrace).build();
+                    Accessor<?> accessor = originalAccessor;
+                    for (JadeRayTraceCallback callback : WailaClientRegistration.instance().rayTraceCallback.callbacks()) {
+                        accessor = callback.onRayTrace(rayTrace, accessor, originalAccessor);
+                    }
+                    if (accessor != null) {
+                        ContraptionExactBlockProvider.INSTANCE.setHit(contraptionEntity, accessor);
+                    }
+                }
+                return rayTrace != null;
+            });
+            return predicateResult != null && !predicateResult.missed();
+        });
     }
 
 }
